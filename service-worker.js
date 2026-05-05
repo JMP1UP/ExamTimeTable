88fb8da5b511c339c78ffbe52d0635b6c6660f13
JMP1UP <tojohnmp@gmail.com>
2026-05-05 16:26:17 +0100

    Update cache name to lhs-exam-timetable-v6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lhs-exam-timetable-v1";
+const CACHE_NAME = "lhs-exam-timetable-v6";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
